d84aac561a8d512b2918c8d6829b736c68e28a79
bombin78 <bombin78@mail.ru>
2020-05-12 01:23:45 +0300

    Подключение исходного шаблона

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ module.exports = {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
@@ -17,10 +18,28 @@ module.exports = {
   loading: { color: '#fff' },
 
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@/static/css/bootstrap.min.css',
+    '@/static/vendors/animate/animate.css',
+    '@/static/vendors/font-awesome/css/font-awesome.min.css',
+    '@/static/vendors/camera-slider/camera.css',
+    //'@/static/vendors/owl_carousel/owl.carousel.css',
+    '@/static/css/style.css',
   ],
 
   plugins: [
+    { src: '~/plugins/js/jquery-1.12.0.min.js', mode: 'client'},
+    //{ src: '~/plugins/js/bootstrap.min.js', mode: 'client'},
+    { src: '~/plugins/vendors/animate/wow.min.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/camera-slider/jquery.easing.1.3.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/camera-slider/camera.min.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/isotope/imagesloaded.pkgd.min.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/isotope/isotope.pkgd.min.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/Counter-Up/jquery.counterup.min.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/Counter-Up/waypoints.min.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/owl_carousel/owl.carousel.min.js', mode: 'client'},
+    //{ src: '~/plugins/vendors/stellar/jquery.stellar.js', mode: 'client'},
+    //{ src: '~/plugins/js/theme.js', mode: 'client'},
     '@/plugins/globals'
   ],
 
