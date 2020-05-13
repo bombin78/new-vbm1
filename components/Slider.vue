5650512b0cb19cf57437be58cc2a8bed839b4662
bombin78 <bombin78@mail.ru>
2020-05-13 20:32:45 +0300

    Разнесение главной и шаблона по компонентам

---
+++ components/Slider.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="slider_area row m0">
+      <div class="slider_inner">
+          <div
+            :data-thumb="item.src"
+            :data-src="item.title"
+            v-for="item of list"
+            :key="item.id">
+              <div class="camera_caption">
+                 <div class="container">
+                      <h5 class=" wow fadeInUp animated">{{item.title}}</h5>
+                      <h3 class=" wow fadeInUp animated" data-wow-delay="0.5s">{{item.subtitle}}</h3>
+                      <p class=" wow fadeInUp animated" data-wow-delay="0.8s">{{item.text}}</p>
+                      <a class=" wow fadeInUp animated" data-wow-delay="1s" href="#">{{item.btnName}}</a>
+                 </div>
+              </div>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted () {
+    // if( $('.slider_inner').length ){
+    //   $('.slider_inner').camera({
+    //     loader: true,
+    //     navigation: true,
+    //     autoPlay:false,
+    //     time: 4000,
+    //     playPause: false,
+    //     pagination: false,
+    //     thumbnails: false,
+    //     overlayer: true,
+    //     hover: false,
+    //     minHeight: '500px',
+    //   });
+    // }
+  },
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          src: '/images/slider-1.jpg',
+          title: 'Welcome to our',
+          subtitle: 'CLEAN, MODERN, MULTIPURPOSE THEME',
+          text: 'Our team of professionals will help you turn your dream home or flat into a reality fast. The Love Boat promises something for everyone. Now the world don\'t move to the beat of just one',
+          btnName: 'Read More',
+        },
+        {
+          id: 2,
+          src: '/images/slider-2.jpg',
+          title: 'Welcome to our',
+          subtitle: 'CLEAN, MODERN, MULTIPURPOSE THEME',
+          text: 'Our team of professionals will help you turn your dream home or flat into a reality fast. The Love Boat promises something for everyone. Now the world don\'t move to the beat of just one',
+          btnName: 'Read More',
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style>
+@import '@/plugins/vendors/camera-slider/camera.css';
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,144 +1,22 @@
 <template>
   <div>
-    <!-- Top Header_Area -->
-    <section class="top_header_area">
-        <div class="container">
-              <ul class="nav navbar-nav top_nav">
-                  <li><a href="#"><i class="fa fa-phone"></i>+1 (168) 314 5016</a></li>
-                  <li><a href="#"><i class="fa fa-envelope-o"></i>info@thethemspro.com</a></li>
-                  <li><a href="#"><i class="fa fa-clock-o"></i>Mon - Sat 12:00 - 20:00</a></li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right social_nav">
-                  <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-              </ul>
-        </div>
-    </section>
-    <!-- End Top Header_Area -->
-
-    <!-- Header_Area -->
-      <nav class="navbar navbar-default header_aera" id="main_navbar">
-          <div class="container">
-              <!-- searchForm -->
-              <div class="searchForm">
-                  <form action="#" class="row m0">
-                      <div class="input-group">
-                          <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                          <input type="search" name="search" class="form-control" placeholder="Type & Hit Enter">
-                          <span class="input-group-addon form_hide"><i class="fa fa-times"></i></span>
-                      </div>
-                  </form>
-              </div><!-- End searchForm -->
-              <!-- Brand and toggle get grouped for better mobile display -->
-              <div class="col-md-2 p0">
-                  <div class="navbar-header">
-                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#min_navbar">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      </button>
-                      <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""></a>
-                  </div>
-              </div>
-
-              <!-- Collect the nav links, forms, and other content for toggling -->
-              <div class="col-md-10 p0">
-                  <div class="collapse navbar-collapse" id="min_navbar">
-                      <ul class="nav navbar-nav navbar-right">
-                          <li class="dropdown submenu">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Home</a>
-                              <ul class="dropdown-menu">
-                                  <li><a href="index.html">Home</a></li>
-                                  <li><a href="index-2.html">Home 2</a></li>
-                              </ul>
-                          </li>
-                          <li class="dropdown submenu">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us</a>
-                              <ul class="dropdown-menu other_dropdwn">
-                                  <li><a href="about.html">About Us</a></li>
-                                  <li><a href="about-2.html">About Us-2</a></li>
-                              </ul>
-                          </li>
-                          <li class="dropdown submenu">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services</a>
-                              <ul class="dropdown-menu other_dropdwn">
-                                  <li><a href="services.html">Services</a></li>
-                                  <li><a href="services-2.html">Services-2</a></li>
-                              </ul>
-                          </li>
-                          <li><a href="#">Gallery</a></li>
-                          <li class="dropdown submenu">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog</a>
-                              <ul class="dropdown-menu">
-                                  <li><a href="blog.html">Blog</a></li>
-                                  <li><a href="blog-2.html">Blog-2</a></li>
-                              </ul>
-                          </li>
-                          <li><a href="contact.html">Contact</a></li>
-                          <li><a href="#" class="nav_searchFrom"><i class="fa fa-search"></i></a></li>
-                      </ul>
-                  </div><!-- /.navbar-collapse -->
-              </div>
-          </div><!-- /.container -->
-      </nav>
-    <!-- End Header_Area -->
-
+    <vbm-top-header-area />
+    <vbm-header-area />
     <nuxt />
-
-    <!-- Footer Area -->
-    <footer class="footer_area">
-        <div class="container">
-            <div class="footer_row row">
-                <div class="col-md-3 col-sm-6 footer_about">
-                    <h2>ABOUT OUR COMPANY</h2>
-                    <img src="images/footer-logo.png" alt="">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <ul class="socail_icon">
-                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3 col-sm-6 footer_about quick">
-                    <h2>Quick links</h2>
-                    <ul class="quick_link">
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Building Construction</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Home Renovation</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Hardwood Flooring</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Repairing Of Roof</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Commercial Construction</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i>Concreate Transport</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3 col-sm-6 footer_about">
-                    <h2>Twitter Feed</h2>
-                    <a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>
-                    <a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>
-                </div>
-                <div class="col-md-3 col-sm-6 footer_about">
-                    <h2>CONTACT US</h2>
-                    <address>
-                        <p>Have questions, comments or just want to say hello:</p>
-                        <ul class="my_address">
-                            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>info@thethemspro.com</a></li>
-                            <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i>+880 123 456 789</a></li>
-                            <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span>Sector # 10, Road # 05, Plot # 31, Uttara, Dhaka 1230 </span></a></li>
-                        </ul>
-                    </address>
-                </div>
-            </div>
-        </div>
-        <div class="copyright_area">
-            Copyright 2017 All rights reserved. Designed by <a href="https://colorlib.com">Colorlib.</a>
-        </div>
-    </footer>
-    <!-- End Footer Area -->
-
+    <vbm-footer-area />
   </div>
 </template>
+
+<script>
+import VbmTopHeader from '@/components/template/TopHeader';
+import VbmHeader from '@/components/template/Header';
+import VbmFooter from '@/components/template/Footer';
+
+export default {
+  componenets: {
+    VbmTopHeaderArea,
+    VbmHeaderArea,
+    VbmFooterArea,
+  }
+}
+</script>
