b51c886d2c02157de1d3e3cb9e9ac0f77b9d59e9
bombin78 <bombin78@mail.ru>
2020-05-19 20:50:09 +0300

    Главная страница: правки + убраны лишние компоненты и стили

--- components/Features.vue
+++ components/Features.vue
@@ -2,7 +2,7 @@
   <section class="our_feature_area">
     <div class="container">
       <div class="tittle wow fadeInUp">
-        <h2>Our Features</h2>
+        <h2>Наши преимущества</h2>
         <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h4>
       </div>
       <div class="feature_row row">
@@ -11,7 +11,7 @@
         </div>
         <div class="col-md-6 feature_content">
           <div class="subtittle">
-            <h2>WHY CHOOSE US</h2>
+            <h2>ПОЧЕМУ ВЫБИРАЮТ НАС</h2>
             <h5>There are many variations of passages of Lorem Ipsum available.</h5>
           </div>
           <div class="media">
@@ -21,7 +21,7 @@
               </a>
             </div>
             <div class="media-body">
-              <a href="#">30+ YEARS OF EXPERIENCE</a>
+              <a href="#">БОЛЕЕ 6 ЛЕТ ОПЫТА РАБОТЫ</a>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting indus-try. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
             </div>
           </div>
@@ -32,7 +32,7 @@
               </a>
             </div>
             <div class="media-body">
-              <a href="#">QUALIFIED EXPERTS</a>
+              <a href="#">КВАЛИФИЦИРОВАННЫЙ СПЕЦИАЛИСТЫ</a>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting indus-try. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
             </div>
           </div>
@@ -43,7 +43,7 @@
               </a>
             </div>
             <div class="media-body">
-              <a href="#">Best Customer Services</a>
+              <a href="#">Лучшее обслуживание клиентов</a>
               <p>Lorem Ipsum is simply dummy text of the printing and typesetting indus-try. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
             </div>
           </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,22 @@
 <template>
-  <div>
+  <main>
     <vbm-slider />
     <vbm-advantages />
-    <vbm-about-us />
-    <vbm-offer />
     <vbm-features />
-    <vbm-services />
-    <vbm-team />
-    <vbm-achievments />
-    <vbm-testimonial />
-    <vbm-featured-works />
-    <vbm-latest-blog />
-    <vbm-partners />
-  </div>
+    <vbm-offer />
+  </main>
 </template>
 
 <script>
 import VbmSlider from '@/components/Slider';
 import VbmAdvantages from '@/components/Advantages';
-import VbmAboutUs from '@/components/AboutUs';
-import VbmOffer from '@/components/Offer';
 import VbmFeatures from '@/components/Features';
-import VbmServices from '@/components/Services';
-import VbmTeam from '@/components/Team';
-import VbmAchievments from '@/components/Achievments';
-import VbmTestimonial from '@/components/Testimonial';
-import VbmFeaturedWorks from '@/components/FeaturedWorks';
-import VbmLatestBlog from '@/components/LatestBlog';
-import VbmPartners from '@/components/Partners';
 
 export default {
   components: {
     VbmSlider,
     VbmAdvantages,
-    VbmAboutUs,
-    VbmOffer,
     VbmFeatures,
-    VbmServices,
-    VbmTeam,
-    VbmAchievments,
-    VbmTestimonial,
-    VbmFeaturedWorks,
-    VbmLatestBlog,
-    VbmPartners,
   },
 }
 </script>
